bf00b96914c86f2de3739984f6d1ec157548d02d
Eoin Graham <eoin.graham@liberty-it.co.uk>
2018-12-02 21:02:58 +0000

    Completed section 4

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,22 +1,29 @@
+import { SummaryPipe } from './piping/summary.pipe';
 import { CoursesService } from './courses.service';
 import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { PipingComponent } from './piping/piping.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    PipingComponent,
+    SummaryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
-    CoursesService
+    CoursesService,
+    SummaryPipe
   ],
   bootstrap: [AppComponent]
 })
--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -5,16 +5,28 @@ import { CoursesService } from './courses.service';
   selector: 'courses',
   template: `
       <h2>{{ getTitle() }}</h2>
+      <img [src]="imageUrl" />
       <ul>
         <li *ngFor="let course of courses">
           {{ course }}
         </li>
+        <button [style.backgroundColor]="buttonActive ? 'blue' : 'white'" class="btn btn-primary" [class.active]="buttonActive">Save</button>
+        <div (click)="onDivClick($event)">
+          <button (click)="onSave($event)" class="btn btn-primary">Click</button>
+        </div>
+        <div>
+          <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+        </div>
       </ul>
   `
 })
 export class CoursesComponent {
   title = "List of courses";
   courses;
+  imageUrl = "";
+  colSpan = 2;
+  buttonActive = true;
+  email = "eoin@example.com";
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
@@ -23,4 +35,18 @@ export class CoursesComponent {
   getTitle() {
     return this.title;
   }
+
+  onDivClick($event) {
+    console.log("Div was clicked", $event);
+  }
+
+  onSave($event) {
+    $event.stopPropagation();
+    console.log("Button was clicked.", $event);
+  }
+
+  onKeyUp() {
+    console.log(this.email);
+  }
+
 }
